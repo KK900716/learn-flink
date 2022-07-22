0bf51f93b627bde8bab81ada2ad5daabf96dbce3
HPME <443808626@qq.com>
2022-07-22 23:38:24 +0800

    1

--- learnFlink1/src/main/java/com/learnFlink/wc/StreamWordCount.java
+++ learnFlink1/src/main/java/com/learnFlink/wc/StreamWordCount.java
@@ -1,6 +1,12 @@
 package com.learnFlink.wc;
 
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 /**
  * Stream
@@ -13,6 +19,24 @@ public class StreamWordCount {
         // 1. 创建流式执行环境
         StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
         // 2. 读取文本流
-        executionEnvironment.socketTextStream("")
+        DataStreamSource<String> lineDataStream = executionEnvironment.socketTextStream("119.3.69.99", 80);
+        // 3. 转换计算
+        SingleOutputStreamOperator<Tuple2<String, Long>> wordAndOneTuple = lineDataStream.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
+            String[] s = line.split(" ");
+            for (String word : s) {
+                out.collect(Tuple2.of(word, 1L));
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.LONG));
+        // 4. 分组
+        KeyedStream<Tuple2<String, Long>, String> tuple2StringKeyedStream = wordAndOneTuple.keyBy(data -> data.f0);
+        // 5. 求和
+        SingleOutputStreamOperator<Tuple2<String, Long>> sum = tuple2StringKeyedStream.sum(1);
+        // 6. 打印
+        sum.print();
+        try {
+            executionEnvironment.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
